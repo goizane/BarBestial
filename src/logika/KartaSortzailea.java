54411c43fd8de3f34526c41a0abe212fef0b9f67
Dell <Dell@10.109.32.118>
2018-04-09 11:23:01 +0200

    aldaketa

--- src/logika/KartaSortzailea.java
+++ src/logika/KartaSortzailea.java
@@ -14,5 +14,49 @@ public class KartaSortzailea {
 		return karta;
 	}
 	
+	public Karta sortuKarta(int zenb){
+		Karta karta=null;
+		if(zenb==0){
+			karta=new KartaHutsa();
+		}else if(zenb==1){
+			karta=new MofetaKarta();
+		}
+		else if(zenb==2){
+			karta=new LoroKarta();
+		}
+		else if(zenb==3){
+			karta=new KanguroKarta();
+		}
+		else if(zenb==4){
+			karta=new TximinoKarta();
+		}
+		else if(zenb==5){
+			karta=new KamaleoiKarta();
+		}
+		else if(zenb==6){
+			karta=new FokaKarta();
+		}
+		else if(zenb==7){
+			karta=new ZebraKarta();
+		}
+		else if(zenb==8){
+			karta=new JirafaKarta();
+		}
+		else if(zenb==9){
+			karta=new SugeKarta();
+		}
+		else if(zenb==10){
+			karta=new KrokodiloKarta();
+		}
+		else if(zenb==11){
+			karta=new HipopotamoKarta();
+		}
+		else if(zenb==12){
+			karta=new LehoiKarta();
+		}
+		return karta;
+		
+	}
+	
 
 }
