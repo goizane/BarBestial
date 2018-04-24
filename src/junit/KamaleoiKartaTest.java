9cab9256e7b37ca0b5e2e177cf0a3946f8083408
Goizane <goizanealonso95@gmail.com>
2018-04-25 00:01:07 +0200

    Aldaketak

---
+++ src/junit/KamaleoiKartaTest.java
@@ -0,0 +1,84 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KamaleoiKartaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAnimaladaEgin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKamaleoiKarta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKopiatzekoKartaEguneratu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKarta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetZenb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetZenb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIrudia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIrudia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsErrekurtsiboa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetErrekurtsiboa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIzena() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIzena() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAnimaladaEginInt() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/junit/LoroKartaTest.java
@@ -0,0 +1,84 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoroKartaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAnimaladaEgin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoroKarta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKenduKarEguneratu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKarta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetZenb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetZenb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIrudia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIrudia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsErrekurtsiboa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetErrekurtsiboa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIzena() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIzena() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAnimaladaEginInt() {
+		fail("Not yet implemented");
+	}
+
+}
